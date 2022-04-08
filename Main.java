37c152426c318c9c8b73af2d91c617aef6631b88
Rodolfo Marques <rodolfo.marques@sundayapp.com>
2022-04-08 18:34:25 +0200

    implementation part 1

--- Main.java
+++ Main.java
@@ -5,14 +5,25 @@ import io.vavr.collection.List;
 
 class Main {
 
-	private static List<Integer> numbers = List.of( 1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+	private static List<Integer> numbers = List.empty();
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		for (Integer i = 2; i < 100; i++) {
+			if (i == 2) {
+				numbers = numbers.append(i);
+				continue;
+			}
 
+			if (i % 2 == 0) { continue; }
 	
+			if (i % (i - 1) == 0) {
+				numbers = numbers.append(i);
+			}
+		}
+		
 
-	System.out.println(numbers);
-   }
+		System.out.println(numbers);
+   	}
 
 }
 
