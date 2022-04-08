c5db66090b3bc46d22839aa6baf3533d43481bea
emilieDelsol <emiliedelsol1@gmail.com>
2022-04-08 18:00:27 +0200

    add vavr.jar

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 import java.util.Arrays;
 
+import io.vavr.collection.List;
+
+
 class Main {
 
-	private static int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	private static List<int> numbers = List.of( 1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
 
     public static void main(String[] args) {
 
 	
-
-
 
 	System.out.println(Arrays.toString(numbers));
    }
