248a52f3876503a7cdfd09e18a3fa3a752c6b2a8
Rodolfo Marques <rodolfo.marques@sundayapp.com>
2022-04-08 18:11:33 +0200

    fixed vavr

--- Main.java
+++ Main.java
@@ -5,13 +5,13 @@ import io.vavr.collection.List;
 
 class Main {
 
-	private static List<int> numbers = List.of( 1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+	private static List<Integer> numbers = List.of( 1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
 
     public static void main(String[] args) {
 
 	
 
-	System.out.println(Arrays.toString(numbers));
+	System.out.println(numbers);
    }
 
 }
