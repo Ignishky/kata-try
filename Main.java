61b936c7a198483f871981078725a5acec134a25
Rodolfo Marques <rodolfo.marques@sundayapp.com>
2022-04-08 17:43:31 +0200

    setup

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Main {
+
+	private static int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+    public static void main(String[] args) {
+
+	
+
+
+
+	System.out.println(Arrays.toString(numbers));
+   }
+
+}
+
